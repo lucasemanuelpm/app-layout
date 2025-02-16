a8496b96c20c862366464bf7562e613b7407a05a
lucasepmartins2008 <lucasepmartins2008@gmail.com>
2025-02-16 22:50:33 +0000

    confirma

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,67 +1,41 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'planet.dart'; // Importando a classe Planet
 
 class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
 
-  DatabaseHelper._init();
-
+  // Inicializa o banco de dados
   Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB('planets.db');
-    return _database!;
+    if (_database != null) {
+      return _database!;
+    } else {
+      _database = await _initDB();
+      return _database!;
+    }
   }
 
-  Future<Database> _initDB(String filePath) async {
+  // Cria o banco de dados e a tabela
+  Future<Database> _initDB() async {
     final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: _createDB,
-    );
+    final path = join(dbPath, 'planets.db');
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
   }
 
-  Future<void> _createDB(Database db, int version) async {
+  // Criação da tabela no banco
+  Future _onCreate(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE planets (
+      CREATE TABLE planets(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        distance_from_sun REAL NOT NULL,
-        size_km REAL NOT NULL,
-        nickname TEXT
+        name TEXT,
+        description TEXT
       )
     ''');
   }
 
-  Future<int> insertPlanet(Map<String, dynamic> planet) async {
-    final db = await instance.database;
-    return await db.insert('planets', planet);
-  }
-
-  Future<List<Map<String, dynamic>>> getPlanets() async {
-    final db = await instance.database;
-    return await db.query('planets');
-  }
-
-  Future<int> updatePlanet(Map<String, dynamic> planet) async {
-    final db = await instance.database;
-    return await db.update(
-      'planets',
-      planet,
-      where: 'id = ?',
-      whereArgs: [planet['id']],
-    );
-  }
-
-  Future<int> deletePlanet(int id) async {
-    final db = await instance.database;
-    return await db.delete(
-      'planets',
-      where: 'id = ?',
-      whereArgs: [id],
-    );
+  // Método para adicionar um planeta no banco
+  Future<int> addPlanet(Planet planet) async {
+    final db = await database;
+    return await db.insert('planets', planet.toMap());
   }
 }
